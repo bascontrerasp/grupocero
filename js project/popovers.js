4cd517557a01605b9cb25e1d6ef94c238b278c42
bascontrerasp <135076672+bascontrerasp@users.noreply.github.com>
2024-06-01 18:14:39 -0400

    Delete js project directory

--- js project/popovers.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var popovers = new bootstrap.Popover(document.body, {
-        selector: '[data-bs-toggle="popover"]',
-        trigger: 'hover'
-    });
-});
